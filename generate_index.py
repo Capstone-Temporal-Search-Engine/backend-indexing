3c245475bf563b5c83ad12005a77ab8ebe9b1937
Alex Tran <at@Alexs-MacBook-Pro.local>
2025-03-05 11:06:24 -0600

    add the functionality to upload dict and post file to s3 once complete indexing

--- generate_index.py
+++ generate_index.py
@@ -1,9 +1,21 @@
 from utils.indexing_utils import generate_index
+from utils.s3_utils import upload_file
 import os
 
+months = input("Enter in the format MM-YYYY: ")
 
-tokenized_files_base_path = os.path.abspath('tokenized_files/02-2027')
-index_files_base_path = os.path.abspath('index_files/02-2027')
+tokenized_files_base_path = os.path.abspath(f'tokenized_files/{months}')
+index_files_base_path = os.path.abspath(f'index_files/{months}')
+# generate_index(tokenized_files_base_path, index_files_base_path)
 
-generate_index(tokenized_files_base_path, index_files_base_path)
+# Open the file in read mode ('r')
+dict_file = open(f'{index_files_base_path}/dict.txt', 'rb')
+post_file = open(f'{index_files_base_path}/post.txt', 'rb')
 
+# (s3_path, file_object, file_name)
+upload_file(f'index_files/{months}', dict_file, "dict.txt")
+upload_file(f'index_files/{months}', post_file, "post.txt")
+
+# Close files after upload
+dict_file.close()
+post_file.close()
